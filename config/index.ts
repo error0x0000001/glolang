8211a84439941d530158e4ed3063c73d7b956c6a
Umashankar Pandey <100037573+error0x0000001@users.noreply.github.com>
2025-06-18 22:07:16 +0530

    Add files via upload
    initial commit

---
+++ config/index.ts
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "GloLang",
+  description:
+    "Interactive platform for language learning with lessons, quizzes, and progress tracking.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "GloLang",
+    "learn-language",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "lingo",
+    "postgresql",
+    "sonner",
+    "drizzle",
+    "zustand",
+    "mysql",
+    "lucide-react",
+    "next-themes",
+    "clerk-themes",
+    "clerk",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Umashankar Pandey",
+    url: "https://github.com/error0x0000001",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "https://github.com/error0x0000001/",
+  email: "",
+} as const;
